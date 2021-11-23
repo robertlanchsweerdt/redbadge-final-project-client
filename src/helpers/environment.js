11c7712c1fb30c7c7d04071b492239b60a40d59b
Robert Lanchsweerdt <robert.lanchsweerdt@gmail.com>
2021-11-22 19:39:31 -0500

    change to environment.js

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,10 +4,10 @@ switch (window.location.hostname) {
   // this is the localhost name of your react app
   case 'localhost' || '127.0.0.1':
     // this is the localhost naem of the API
-    APIURL = `http://localhost:4000`;
+    APIURL = 'http://localhost:4000';
     break;
   case 'bedrockhillshoa.herokuapp.com':
-    APIURL = `https://bedrockhillshoa.herokuapp.com`;
+    APIURL = 'https://bedrockhillshoa.herokuapp.com';
 }
 
 export default APIURL;
